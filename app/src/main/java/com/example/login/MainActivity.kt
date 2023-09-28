46a5410f503b2d4e6cbd4e6429751d70039da74b
Varoni <varonidiogo@gmail.com>
2023-09-27 22:16:57 -0300

    Implementação binding botão 'Entrar'

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.login
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.hide()
         window.statusBarColor = Color.parseColor("#FFFFFF")
+
+        binding.btEntrar.setOnClickListener {
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when {
+                email.isEmpty() -> {
+                    binding.editEmail.error = "Preencha o E-mail!"
+                }
+                senha.isEmpty() -> {
+                    binding.editSenha.error = "Preencha a Senha!"
+                }
+            }
+        }
     }
 }
